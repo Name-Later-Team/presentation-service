a371a413cd8232341950f602be18099582d64724
Anh Le <lehoanganh.le2001@gmail.com>
2023-04-17 00:18:56 +0700

    complete ci/cd script

---
+++ test/unit/app.controller.spec.ts
@@ -0,0 +1,33 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { AppController } from "src/app.controller";
+import { AppService } from "src/app.service";
+import { DataResponse } from "src/core/response";
+import { PRESENTATION_ACTION_PUB_TOKEN } from "src/infrastructure/brokers/publishers";
+
+describe("AppController", () => {
+    let appController: AppController;
+
+    beforeEach(async () => {
+        const app: TestingModule = await Test.createTestingModule({
+            controllers: [AppController],
+            providers: [
+                AppService,
+                {
+                    provide: PRESENTATION_ACTION_PUB_TOKEN,
+                    useValue: {
+                        getResult: jest.fn(),
+                    },
+                },
+            ],
+        }).compile();
+
+        appController = app.get<AppController>(AppController);
+    });
+
+    describe("root", () => {
+        it("should return service information (name and version)", () => {
+            const resp = new DataResponse({ service: "Presentation Service", version: "v1" });
+            expect(appController.getHomePage()).toEqual(resp);
+        });
+    });
+});
